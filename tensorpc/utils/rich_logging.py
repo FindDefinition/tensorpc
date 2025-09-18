9abae72d2c4702ca5054ecfb885415495b3ce71e
yan.yan <yanyan.sub@outlook.com>
2025-09-18 03:34:03 +0000

    fix missing log

--- tensorpc/utils/rich_logging.py
+++ tensorpc/utils/rich_logging.py
@@ -1,7 +1,9 @@
+from typing import Optional
 from rich.logging import RichHandler
 from tensorpc.constants import TENSORPC_ENABLE_RICH_LOG
 import logging 
-
+from logging import Handler, LogRecord
+from rich.text import Text
 
 TENSORPC_LOGGING_OVERRIDED_PATH_LINENO_KEY = "__tensorpc_overrided_path_lineno_key"
 
@@ -15,6 +17,21 @@ class ModifiedRichHandler(RichHandler):
     """A custom rich handler that add ability to override the path and 
     lineno of the log record.
     """
+    def get_level_text(self, record: LogRecord):
+        """Get the level name from the record.
+
+        Args:
+            record (LogRecord): LogRecord instance.
+
+        Returns:
+            Text: A tuple of the style and level name.
+        """
+        level_name = record.levelname
+        level_text = Text.styled(
+            level_name[:1], f"logging.level.{level_name.lower()}"
+        )
+        return level_text
+
     def emit(self, record):
         if hasattr(record, TENSORPC_LOGGING_OVERRIDED_PATH_LINENO_KEY):
             path_lineno = getattr(record, TENSORPC_LOGGING_OVERRIDED_PATH_LINENO_KEY)
@@ -24,7 +41,7 @@ class ModifiedRichHandler(RichHandler):
                     record.lineno = path_lineno[1]
         super().emit(record)
 
-def get_logger(name: str, level: str = "WARNING", show_path: bool = False) -> logging.Logger:
+def get_logger(name: str, level: str = "WARNING", show_path: bool = False, log_time_format: str = "[%X]") -> logging.Logger:
     logger = logging.getLogger(name)
     if logger.handlers:
         for handler in logger.handlers:
@@ -34,7 +51,7 @@ def get_logger(name: str, level: str = "WARNING", show_path: bool = False) -> lo
     logger.setLevel(level)
     formatter = logging.Formatter("%(name)s|%(message)s")
     if TENSORPC_ENABLE_RICH_LOG:
-        rh = ModifiedRichHandler(rich_tracebacks=True, show_path=show_path)
+        rh = ModifiedRichHandler(rich_tracebacks=True, show_path=show_path, log_time_format=log_time_format)
         rh.setFormatter(formatter)
         logger.addHandler(rh)
     else:
